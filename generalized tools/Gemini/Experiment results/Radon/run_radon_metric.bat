233d1ca295030ac29ba26b6f4262783b692b807a
Parasite-231 <muktadir@iut-dhaka.edu>
2024-01-03 03:56:10 +0600

    updated radon results

--- generalized tools/Gemini/Experiment results/Radon/run_radon_metric.bat
+++ generalized tools/Gemini/Experiment results/Radon/run_radon_metric.bat
@@ -2,13 +2,13 @@
 setlocal enabledelayedexpansion
 
 REM Create a directory for the output files (if it doesn't exist)
-mkdir output_files 2>nul
+mkdir radon_output 2>nul
 
 REM Set the range of index numbers (0 to 125)
 for /l %%i in (0, 1, 125) do (
     set index=%%i
-    set file=generated_code_!index!.py
-    set output=output_files\output_!index!.txt
+    set file="C:\Users\pc\Documents\Case-Study-on-Code-Quality-of-AI-Assisted-Code-Generation-Tools\generalized tools\Gemini\Generated Codes\generated_code_!index!.py"
+    set output=radon_output\output_!index!.txt
 
     REM Run Radon commands for each file and save output to a text file
     echo Calculating Cyclomatic Complexity for !file!
